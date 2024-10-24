8b58e1c82fe6a7c8d2dae3c9d41b18ef345d33f2
student17 <student17@teb.pl>
2024-10-24 14:09:06 +0200

    commit 4

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,4 @@
+
 from django.urls import path
 from account import views
 from django.contrib.auth import views as auth_views
@@ -6,6 +7,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     # path('login/', views.user_login, name="login")
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LoginView.as_view(), name='logout'),
-
+    path('logout/', auth_views.LogoutView .as_view(), name='logout'),
+    path('register/', views.register, name="register"),
+    path('', views.dashboard, name='dashboard')
 ]
